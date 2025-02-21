ba049d152fab78f89e983aec3e51260b214859a4
Penar Musaraj <pmusaraj@gmail.com>
2025-02-21 16:04:15 -0500

    try

--- api/capture.js
+++ api/capture.js
@@ -1,17 +1,17 @@
-const playwright = require("playwright-aws-lambda");
 const { chromium: playwright } = require("playwright-core");
 const chromium = require("@sparticuz/chromium");
 
 module.exports = async (req, res) => {
   const { query } = req;
+  const browser = await playwright.launch({
+    args: chromium.args,
+    executablePath: await chromium.executablePath(),
+    headless: true,
+  });
+  const context = await browser.newContext();
+
   try {
     if (query.url && isValidUrl(query.url)) {
-      const browser = await playwright.launch({
-        args: chromium.args,
-        executablePath: await chromium.executablePath(),
-        headless: chromium.headless,
-      });
-      const context = await browser.newContext();
       const page = await context.newPage();
       await page.goto(query.url);
       const screenshot = await page.screenshot({ type: "png" });
@@ -26,6 +26,7 @@ module.exports = async (req, res) => {
     });
   } finally {
     if (browser !== null) {
+      await context.close();
       await browser.close();
     }
   }
